cceed52849ca5a07a634f7a1af0b777b6cdf4f8c
Nishi <nishi@Nishis-MacBook-Pro.local>
2020-03-22 19:00:53 +0900

    API処理リファクタリング

--- source/src/middlewares/YoutubeAPIToolSaga.js
+++ source/src/middlewares/YoutubeAPIToolSaga.js
@@ -1,17 +1,30 @@
 import * as actionTypes from '../consts/YoutubeAPIToolActionType';
+import * as YoutubeAPIToolURL from '../consts/YoutubeAPIToolURL';
 import { put, call, takeEvery } from 'redux-saga/effects';
-import getYoutubeMovieInfoList from '../utils/api';
+import * as api from '../utils/api';
 
-function* fetchList(action) {
-    // call関数を使用してgetYoutubeMovieInfoListを実行して完了するまで待つ
-    const result = yield call(getYoutubeMovieInfoList, action.searchCondition);
+function* getYoutubeMovieInfoList(action) {
+    // URL作成
+    let url = YoutubeAPIToolURL.YOUTUBE_SEARCH_API + 
+        process.env.REACT_APP_YOUTUBE_API_KEY +
+        '&part=' + action.searchCondition.part +
+        (action.searchCondition.channelid != '' ? '&channelid=' + action.searchCondition.channelid : '') +
+        (action.searchCondition.channelType != '' ? '&channelType=' + action.searchCondition.channelType : '') +
+        (action.searchCondition.eventType != '' ? '&eventType=' + action.searchCondition.eventType : '') +
+        (action.searchCondition.maxResults != '' ? '&maxResults=' + action.searchCondition.maxResults : '') +
+        (action.searchCondition.order != '' ? '&order=' + action.searchCondition.order : '') +
+        (action.searchCondition.q != '' ? '&q=' + action.searchCondition.q : '')
+
+    // call関数を使用してapi.getAPIを実行して完了するまで待つ
+    let result = yield call(api.getAPI, url);
+
     // put関数を使用してactionをdispatch
     yield put({
         type: actionTypes.SEARCH_SUCCSES, result: result
     });
 }
 
-// takeEvery関数を使用して「EXECUTE_SEARCH」のアクションが実行される度にfetchListを実行する
-const YoutubeAPIToolSaga = [takeEvery(actionTypes.EXECUTE_SEARCH, fetchList)];
+// takeEvery関数を使用して「EXECUTE_SEARCH」のアクションが実行される度にgetYoutubeMovieInfoListを実行する
+const YoutubeAPIToolSaga = [takeEvery(actionTypes.EXECUTE_SEARCH, getYoutubeMovieInfoList)];
 
 export default YoutubeAPIToolSaga;
--- source/src/utils/api.js
+++ source/src/utils/api.js
@@ -1,24 +1,14 @@
 import axios from 'axios';
 
-function getYoutubeMovieInfoList(searchCondition) {
+export function getAPI(url) {
     return axios.get(
-        'https://www.googleapis.com/youtube/v3/search?' +
-        'key=AIzaSyCavU3u6Ekj1cYjdG0SHsSTmoYqKwKR92E' +
-        '&part=' + searchCondition.part +
-        (searchCondition.channelid != '' ? '&channelid=' + searchCondition.channelid : '') +
-        (searchCondition.channelType != '' ? '&channelType=' + searchCondition.channelType : '') +
-        (searchCondition.eventType != '' ? '&eventType=' + searchCondition.eventType : '') +
-        (searchCondition.maxResults != '' ? '&maxResults=' + searchCondition.maxResults : '') +
-        (searchCondition.order != '' ? '&order=' + searchCondition.order : '') +
-        (searchCondition.q != '' ? '&q=' + searchCondition.q : '')
-        ).then((result) => {
-            return JSON.stringify(result.data, null, 4);
-        }).catch((error) => {
-            return (
-                'エラーコード：' + error.response.data.error.code + 
-                ' エラーメッセージ：' + error.response.data.error.message
-            );
-        });
+        url
+    ).then((result) => {
+        return JSON.stringify(result.data);
+    }).catch((error) => {
+        return (
+            'errorCode:' + error.response.data.error.code +
+            'errorMessage' + error.response.data.error.message
+        );
+    });
 }
-
-export default getYoutubeMovieInfoList;
